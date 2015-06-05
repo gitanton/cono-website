aadcafb5694e979d7ac77c8f42342147581673b0
WenjiaChen <chenwenjia@chenwenjiadeMacBook-Pro.local>
2015-06-05 17:53:30 +0800

    adjust this link to smooth scroll

--- www/app/scripts/app.js
+++ www/app/scripts/app.js
@@ -17,7 +17,8 @@ angular
         'ngResource',
         'ngRoute',
         'ngSanitize',
-        'ngTouch'
+        'ngTouch',
+        'angularSmoothscroll'
     ])
     .config(function ($routeProvider) {
         $routeProvider
